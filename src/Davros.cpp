d1b0153b463b2028520dc194f5683e1f0d2370be
James Ward <j.ward@acfr.usyd.edu.au>
2014-01-21 19:17:38 +1100

    Split Davros.cpp to have a separate header Davros.h.

--- src/Davros.cpp
+++ src/Davros.cpp
@@ -1,49 +1,44 @@
-#include "WPILib.h"
 #include "CommandBase.h"
 
-class Davros : public IterativeRobot {
-private:
-	Command *autonomousCommand;
-	LiveWindow *lw;
-	
-	void RobotInit() {
-		CommandBase::init();
-		autonomousCommand = NULL;
-		lw = LiveWindow::GetInstance();
-		
-		lw->AddActuator("Chassis", "Motor A", CommandBase::chassis->driveMotorA);
-		lw->AddActuator("Chassis", "Motor B", CommandBase::chassis->driveMotorB);
-		lw->AddActuator("Chassis", "Motor C", CommandBase::chassis->driveMotorC);
-		lw->AddActuator("Chassis", "Motor D", CommandBase::chassis->driveMotorD);
-        
-        SmartDashboard::PutData(Scheduler::GetInstance());
-        SmartDashboard::PutData(CommandBase::chassis);
-	}
-	
-	void AutonomousInit() {
-		//autonomousCommand->Start();
-	}
-	
-	void AutonomousPeriodic() {
-		Scheduler::GetInstance()->Run();
-	}
-	
-	void TeleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to 
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		//autonomousCommand->Cancel();
-	}
-	
-	void TeleopPeriodic() {
-		Scheduler::GetInstance()->Run();
-	}
-	
-	void TestPeriodic() {
-		lw->Run();
-	}
-};
+#include "Davros.h"
+
+void Davros::RobotInit() {
+    CommandBase::init();
+    autonomousCommand = NULL;
+    lw = LiveWindow::GetInstance();
+    
+    lw->AddActuator("Chassis", "Motor A", CommandBase::chassis->driveMotorA);
+    lw->AddActuator("Chassis", "Motor B", CommandBase::chassis->driveMotorB);
+    lw->AddActuator("Chassis", "Motor C", CommandBase::chassis->driveMotorC);
+    lw->AddActuator("Chassis", "Motor D", CommandBase::chassis->driveMotorD);
+    
+    SmartDashboard::PutData(Scheduler::GetInstance());
+    SmartDashboard::PutData(CommandBase::chassis);
+}
+
+void Davros::AutonomousInit() {
+    //autonomousCommand->Start();
+}
+
+void Davros::AutonomousPeriodic() {
+    Scheduler::GetInstance()->Run();
+}
+
+void Davros::TeleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to 
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    //autonomousCommand->Cancel();
+}
+
+void Davros::TeleopPeriodic() {
+    Scheduler::GetInstance()->Run();
+}
+
+void Davros::TestPeriodic() {
+    lw->Run();
+}
 
 START_ROBOT_CLASS(Davros);
 
---
+++ src/Davros.h
@@ -0,0 +1,20 @@
+#ifndef Davros_h
+#define Davros_h
+
+#include "WPILib.h"
+
+class Davros : public IterativeRobot {
+    private:
+        Command *autonomousCommand;
+        LiveWindow *lw;
+        
+    public:
+        void RobotInit();
+        void AutonomousInit();
+        void AutonomousPeriodic();
+        void TeleopInit();
+        void TeleopPeriodic();
+        void TestPeriodic();
+};
+
+#endif
